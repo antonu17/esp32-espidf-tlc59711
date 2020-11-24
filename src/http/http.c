b3f11f3c023832e985864061ea1e3b47de67e1e9
Anton Ustyuzhanin <antonu17@gmail.com>
2020-11-24 22:30:00 +0100

    Update mDNS service

--- src/http/http.c
+++ src/http/http.c
@@ -1,49 +1,35 @@
-/* HTTP Restful API Server Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <driver/gpio.h>
 #include <esp_event.h>
 #include <esp_log.h>
 #include <esp_netif.h>
-#include <lwip/apps/netbiosns.h>
 #include <mdns.h>
-#include <nvs_flash.h>
-#include <sdmmc_cmd.h>
 
+#include "../nvs.h"
 #include "sdkconfig.h"
 #include "spiffs.h"
-
-#define MDNS_INSTANCE "esp home web server"
 
 static const char *TAG = __FILE__;
 
 esp_err_t start_rest_server(const char *base_path);
 
 static void initialise_mdns(void) {
-    mdns_init();
-    mdns_hostname_set("esp-home");
-    mdns_instance_name_set(MDNS_INSTANCE);
+    ESP_ERROR_CHECK(mdns_init());
+    ESP_ERROR_CHECK(mdns_hostname_set("rgb-cube"));
+    ESP_ERROR_CHECK(mdns_instance_name_set("RGB LED Cube (awesome)"));
 
     mdns_txt_item_t serviceTxtData[] = {
         {"board", "esp32"},
         {"path", "/"}};
 
     ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, serviceTxtData,
-                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+                                     sizeof serviceTxtData / sizeof serviceTxtData[0]));
+    ESP_LOGI(TAG, "mDNS initialized");
 }
 
 void init_http(void) {
-    ESP_ERROR_CHECK(nvs_flash_init());
+    init_nvs();
     ESP_ERROR_CHECK(esp_netif_init());
 
     initialise_mdns();
-    netbiosns_init();
-    netbiosns_set_name("esp-home");
 
     ESP_ERROR_CHECK(start_rest_server("/spiffs"));
     list_dir("/spiffs");
