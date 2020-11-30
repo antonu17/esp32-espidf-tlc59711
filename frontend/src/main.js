89abdcb4bc005d43c5465f84a885d2c5c77682ff
Anton Ustyuzhanin <antonu17@gmail.com>
2020-11-30 20:22:42 +0100

    Configure webpack to split bundles

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from 'axios'
 
+Vue.config.devtools = false
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,5 +10,24 @@ module.exports = {
         ws: true
       }
     }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[id].bundle.js'
+    },
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        maxSize: 200000,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name() {
+              return `v`;
+            }
+          }
+        }
+      }
+    }
   }
 }
