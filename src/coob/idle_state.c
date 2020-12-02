fdf9e2f410a0b8ea512c5bb9cc2e818025582c7d
Anton Ustyuzhanin <antonu17@gmail.com>
2020-12-02 21:08:11 +0100

    Allow effect switching while idle

--- src/coob/idle_state.c
+++ src/coob/idle_state.c
@@ -6,11 +6,13 @@
 
 #include "coob_state.h"
 #include "draw.h"
+#include "effect_list.h"
 #include "framebuffer.h"
 
 /* Possible transition to the following state: */
 #include "loop_state.h"
 #include "solo_state.h"
+#include "switching_state.h"
 
 static void start_idle_mode();
 static void stop_idle_mode();
@@ -27,12 +29,24 @@ static void start_solo(coob_state_t state) {
     transition_to_solo(state);
 }
 
+static coob_err_t switch_effect(coob_state_t state, char *effect_name) {
+    effect_t *e = NULL;
+    e = effect_list_get_by_name(effect_list, effect_name);
+    if (NULL == e) {
+        ESP_LOGD(__FILE__, "effect not found: %s", effect_name);
+        return COOB_ERR_EFFECT_NOT_FOUND;
+    }
+    transition_to_switching(state, e, NULL);
+    return COOB_ERR_OK;
+}
+
 void transition_to_idle(coob_state_t state) {
     start_idle_mode();
     default_state(state);
     state->name = "idle";
     state->loop = start_loop;
     state->solo = start_solo;
+    state->switch_effect = switch_effect;
     state->current_mode = IDLE;
     ESP_LOGD(__FILE__, "switched to idle mode");
 }
